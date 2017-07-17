b294a71388df587678b9ff6b7dc50a7235902c3a
Rick Fleuren <info@rickfleuren.nl>
2017-07-17 13:33:54 +0200

    Now with state of tree

--- theatre/src/main/resources/static/js/treeview.js
+++ theatre/src/main/resources/static/js/treeview.js
@@ -1,15 +1,66 @@
+treeview = new Object();
+
 $(function () {
-  $(".nav-tree a").click(toggleNode);
+  treeview.restoreData();
+  $(".nav-tree a").click(treeview.toggleNode);
 });
 
-function toggleNode() {
-  var parent = $(this).parent();
-  var ul = parent.find("ul");
+treeview.toggleNode = function () {
+  var li = $(this).parent();
+  li.toggleClass("tree-collapsed")
+  var ul = li.find("ul");
   ul.toggleClass("hide");
 
-  var icon = parent.find("i");
+  var icon = li.find("i");
   icon.toggleClass("fa-minus-square");
   icon.toggleClass("fa-plus-square");
 
+  treeview.saveData();
+
   return false;
 }
+
+treeview.getKey = function getId(id) {
+  return "navtree_html_" + id;
+}
+
+treeview.saveData = function () {
+  if (!sessionStorage) {
+    return;
+  }
+
+  $(".nav-tree").each(function () {
+    var id = $(this).attr("id");
+    if (!id) {
+      return;
+    }
+
+    var data = {
+      id: id,
+      html: $(this).html()
+    };
+
+    sessionStorage.setItem(treeview.getKey(id), JSON.stringify(data));
+  });
+};
+
+treeview.restoreData = function () {
+  if (!sessionStorage) {
+    return;
+  }
+
+  $(".nav-tree").each(function () {
+    var id = $(this).attr("id");
+    if (!id) {
+      return;
+    }
+
+    var key = treeview.getKey(id);
+    var data = sessionStorage.getItem(key);
+    if (!data) {
+      return null;
+    }
+    $(this).html(JSON.parse(data).html);
+    sessionStorage.removeItem(key);
+  });
+};
